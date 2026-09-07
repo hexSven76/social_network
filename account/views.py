b9d117ce2e28d71252b423965a697a15da446f5a
Sven <sajad.var83@gmail.com>
2026-09-07 12:07:48 +0330

    account app and register.html created.

---
+++ account/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.views import View
+from .forms import UserRegisterationForm
+
+
+class RegisterView(View):
+    
+    def get(self, request):
+        form = UserRegisterationForm()
+        return render(request, 'account/register.html', {"form":form})
+
+    def post(self, request):
+        pass
