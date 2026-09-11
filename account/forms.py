11769066bedd84e921290973cbc25dad1417db67
Sven <sajad.var83@gmail.com>
2026-09-11 19:31:17 +0330

    added clean_email() and clean_username() to account/forms.py for validating unique email/username.

--- account/forms.py
+++ account/forms.py
@@ -1,7 +1,23 @@
 from django import forms
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 
 class UserRegisterationForm(forms.Form):
 
     username = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'your username'}))
     email = forms.EmailField(widget=forms.EmailInput(attrs={'class':'form-control', 'placeholder':'example@mail.domain'}))
-    password = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control', 'placeholder':'your password'})) 
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control', 'placeholder':'your password'}))
+
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        user = User.objects.filter(email=email).exists()
+        if user:
+            raise ValidationError('This Email Already Exists!')
+        return email
+    
+    def clean_username(self):
+        username = self.cleaned_data['username']
+        user = User.objects.filter(username=username).exists()
+        if user:
+            raise ValidationError('This username Already Exists!')
+        return username
