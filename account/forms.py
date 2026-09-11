418ee2e6f9812149d18d85b6fa64711bf3e636c3
Sven <sajad.var83@gmail.com>
2026-09-11 19:46:52 +0330

    added confirm password field for registeration, and overrided clean() for custom valdiation check.

--- account/forms.py
+++ account/forms.py
@@ -6,7 +6,8 @@ class UserRegisterationForm(forms.Form):
 
     username = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'your username'}))
     email = forms.EmailField(widget=forms.EmailInput(attrs={'class':'form-control', 'placeholder':'example@mail.domain'}))
-    password = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control', 'placeholder':'your password'}))
+    password1 = forms.CharField(label='password', widget=forms.PasswordInput(attrs={'class':'form-control', 'placeholder':'your password'}))
+    password2 = forms.CharField(label='confirm password', widget=forms.PasswordInput(attrs={'class':'form-control', 'placeholder':'your password again'}))
 
     def clean_email(self):
         email = self.cleaned_data['email']
@@ -21,3 +22,12 @@ class UserRegisterationForm(forms.Form):
         if user:
             raise ValidationError('This username Already Exists!')
         return username
+    
+    # overriding clean() for custom validation (password = confirm password)
+    def clean(self):
+        cd = super().clean()
+        pw1 = cd.get('password1')
+        pw2 = cd.get('password2')
+        if pw1 and pw2: # checking if they exist
+            if pw1 != pw2:
+                raise ValidationError('Passwords must match!')
