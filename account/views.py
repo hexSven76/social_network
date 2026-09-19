eeb13a3811d06ecfac0c51ad8fb8779ab8dae8e7
Sven <sajad.var83@gmail.com>
2026-09-19 20:52:14 +0330

    created profile page

--- account/views.py
+++ account/views.py
@@ -70,3 +70,10 @@ class UserLogoutView(LoginRequiredMixin, View):
         logout(request)
         messages.success(request, 'Logged out Successfully!', 'success')
         return redirect('home:home')
+
+
+class UserProfileView(LoginRequiredMixin, View):
+
+    def get(self, request, user_id):
+        user = User.objects.get(id=user_id)
+        return render(request, 'account/profile.html', {'user':user})
