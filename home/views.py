8368a525cb5d313aeff627a0a1ff8167923f7845
Sven <sajad.var83@gmail.com>
2026-09-24 14:32:22 +0330

    posts now visible in homepage and user profiles.

--- home/views.py
+++ home/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 from django.views import View
+from .models import Post
+
 
 class HomeView(View):
     
     def get(self, request):
-        return render(request, 'home/index.html')
-    
-    def post(self, request):
-        return render(request, 'home/index.html')
+        posts = Post.objects.all()
+        return render(request, 'home/index.html', {'posts':posts})
+
+
+class PostDetailView(View):
+
+    def get(self, request, post_id, post_slug):
+        post = Post.objects.get(id=post_id, slug=post_slug)
+        return render(request, 'home/detail.html', {'post':post})
