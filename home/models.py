6652f136e0212941058c44e506ee54928c7fd3b2
Sven <sajad.var83@gmail.com>
2026-09-26 14:09:50 +0330

    added get_absolute_url() for post urls

--- home/models.py
+++ home/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 
 class Post(models.Model):
@@ -10,5 +11,10 @@ class Post(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
+
     def __str__(self):
         return f'{self.slug} - {self.updated}'
+    
+
+    def get_absolute_url(self):
+        return reverse('home:post_detail', args=(self.id, self.slug))
